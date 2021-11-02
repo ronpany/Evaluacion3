9431cd7c1d368c197d8cce53422063b69db6e353
Ronny Andres <76263130+ronpany@users.noreply.github.com>
2021-11-02 10:14:48 -0500

    Apliacion completa, Faltan los Estilos

--- src/evaluacion3/FXMLDocumentController.java
+++ src/evaluacion3/FXMLDocumentController.java
@@ -7,9 +7,12 @@ package evaluacion3;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -99,6 +102,7 @@ public class FXMLDocumentController implements Initializable {
     ObservableList<Plato> Totales = FXCollections.observableArrayList();
     
     
+    
     // ------------------------------   CREACION DE PLATOS --------------------------------
     // Validar Datos TxtFields
     public boolean validarDatosCP(){
@@ -133,22 +137,22 @@ public class FXMLDocumentController implements Initializable {
         
         Lst_Entrada.getItems().clear();
         PlatosEntrada.forEach((p) -> {
-            Lst_Entrada.getItems().add(new MenuItem(p.getName()+"; $"+p.getPrice()));
+            Lst_Entrada.getItems().add(new MenuItem(p.getName()));
         });
         
         Lst_PlatoFuerte.getItems().clear();
         PlatosFuerte.forEach((pf)->{
-        Lst_PlatoFuerte.getItems().add(new MenuItem(pf.getName()+"; $"+pf.getPrice()));
+        Lst_PlatoFuerte.getItems().add(new MenuItem(pf.getName()));
         });
         
         Lst_Postre.getItems().clear();
         Postres.forEach((pp) -> {
-        Lst_Postre.getItems().add(new MenuItem(pp.getName()+"; $"+pp.getPrice()));
+        Lst_Postre.getItems().add(new MenuItem(pp.getName()));
         });
         
         Lst_Bebida.getItems().clear();
         Bebidas.forEach((pb) -> {
-        Lst_Bebida.getItems().add(new MenuItem(pb.getName()+"; $"+pb.getPrice()));
+        Lst_Bebida.getItems().add(new MenuItem(pb.getName()));
         });
         
         
@@ -156,12 +160,18 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-    Lst_Entrada.setText("Ninguna");
-    Lst_Bebida.setText("Ninguna");
-    Lst_PlatoFuerte.setText("Ninguna");
-    Lst_Postre.setText("Ninguna");
+    Txt_CantidadEntrada.setText("0");
+    Txt_CantidadPlatoFuerte.setText("0");
+    Txt_CantidadPostre.setText("0");
+    Txt_CantidadBebida.setText("0");
+    
+    Lst_Entrada.setText("Ninguno");
+    Lst_Bebida.setText("Ninguno");
+    Lst_PlatoFuerte.setText("Ninguno");
+    Lst_Postre.setText("Ninguno");
     Btn_CrearPlato.setOnAction((event) -> {
-        String category="";
+        try {
+         String category="";
         if(validarDatosCP()){
             if(Txt_Precio.getText().matches("[0-9.]+")){
             if(Rbtn_Entrada.isSelected()){
@@ -193,17 +203,118 @@ public class FXMLDocumentController implements Initializable {
         }else{JOptionPane.showMessageDialog(null, "Ingrese SOLO numeros en el PRECIO!");}
         }else{JOptionPane.showMessageDialog(null, "Ingrese los Datos!");
               Txt_Nombre.requestFocus();}
-        cargarPlatosP();
+        cargarPlatosP();    
+        } catch (NullPointerException e) {
+            JOptionPane.showMessageDialog(null, "Ingrese solo NUMEROS en el Precio!");
+        }
+       
     });
     
-    Lst_Entrada.setOnAction((event) -> {
-        Lst_Entrada.getItems().get(1).setOnAction((ev) -> {
-            System.out.println("SISA");
-        //Lst_Entrada.setText(Lst_Entrada.getItems().get(0).getText());
+    Lst_Entrada.setOnMouseClicked((event) -> {
+        for (int i = 0; i < PlatosEntrada.size(); i++) {
+            //Lst_Entrada.getItems().get(i).setOnAction((e) -> {
+            String s = Lst_Entrada.getItems().get(i).getText();
+            Lst_Entrada.getItems().get(i).setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                Lst_Entrada.setText(s);
+                }
+            }); 
+        }  
+    });
+    
+    Lst_PlatoFuerte.setOnMouseClicked((event) -> {
+        for (int i = 0; i < PlatosFuerte.size(); i++) {
+            //Lst_Entrada.getItems().get(i).setOnAction((e) -> {
+            String s = Lst_PlatoFuerte.getItems().get(i).getText();
+            Lst_PlatoFuerte.getItems().get(i).setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                Lst_PlatoFuerte.setText(s);
+                }
+            }); 
+        }  
+    });
+    
+    Lst_Postre.setOnMouseClicked((event) -> {
+        for (int i = 0; i < Postres.size(); i++) {
+            //Lst_Entrada.getItems().get(i).setOnAction((e) -> {
+            String s = Lst_Postre.getItems().get(i).getText();
+            Lst_Postre.getItems().get(i).setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                Lst_Postre.setText(s);
+                }
+            }); 
+        }  
+    });
+    
+    Lst_Bebida.setOnMouseClicked((event) -> {
+        for (int i = 0; i < Bebidas.size(); i++) {
+            //Lst_Entrada.getItems().get(i).setOnAction((e) -> {
+            String s = Lst_Bebida.getItems().get(i).getText();
+            Lst_Bebida.getItems().get(i).setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                Lst_Bebida.setText(s);
+                }
+            }); 
+        }  
+    });
+    
+    Btn_FacturaPedido.setOnAction((event) -> {
         
-        });
+        try {
+          ArrayList names = new ArrayList<String>();
+       ArrayList amount = new ArrayList<Integer>();
+       
+       names.add("Plato de Entrada:  "+Lst_Entrada.getText());
+       names.add("Plato Fuerte:      "+Lst_PlatoFuerte.getText());
+       names.add("Postre:            "+Lst_Postre.getText());
+       names.add("Bebida:            "+Lst_Bebida.getText());
+       
+       amount.add(Integer.parseInt(Txt_CantidadEntrada.getText()));
+       amount.add(Integer.parseInt(Txt_CantidadPlatoFuerte.getText()));
+       amount.add(Integer.parseInt(Txt_CantidadPostre.getText()));
+       amount.add(Integer.parseInt(Txt_CantidadBebida.getText()));  
+
+       
+       
+        String factura="";
+        float total=0;
+        
+        for (int i = 0; i <=3; i++) {
+            if(!names.get(i).equals("Ninguno")) {
+               if ((int)amount.get(i)!=0) {
+                   factura += (String)names.get(i)+" Cantidad: "+String.valueOf(amount.get(i))+"\n";  
+                   for (Plato p : Totales) {
+                       String n = names.get(i).toString().substring(names.get(i).toString().length()-p.getName().length(), names.get(i).toString().length());
+                      if(p.getName().equals(n)){
+                          total += p.getPrice()*Float.parseFloat(amount.get(i).toString());
+                      } 
+                   }
+               }
+           }
+
+        }
+        if(factura.equals("")){
+            factura="\n\n  No hay pedidos!";
+
+            TxtArea_Factura.setText(factura);
+        }else{
+            if(Check_ClienteFiel.isSelected()==true){
+                factura+="\n"+"Valor Parcial del Peido:  "+total+"\n\n";
+                factura+="Por ser Cliente Fiel de Nuestra Tienda Tiene un Descuento del 12%\nValor Total del Pedido: $"+(total*0.88);
+                TxtArea_Factura.setText(factura);
+            }else{factura+="\n"+"Valor Total del Peido:  $"+total+"\n\n";
+                TxtArea_Factura.setText(factura);}
+        }    
+        
+          
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Debe Ingresar Solo NUMEROS en las Cantidades!");
+        }
+
+       
+       
+       
     });
-    
     
     
     } 
